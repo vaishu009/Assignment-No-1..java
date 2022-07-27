49e75e475651cd42e13c031c6c28067f34deb228
vaishu009 <109944617+vaishu009@users.noreply.github.com>
2022-07-27 12:00:36 +0530

    Calculate Factorial of a Number Using Recursion

---
+++ calculatefactorial.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaprograms;
+
+/**
+ *
+ * @author kanev
+ */
+public class calculatefactorial {
+     public static void main(String[] args) {
+        int num = 6;
+        long factorial = multiplyNumbers(num);
+        System.out.println("Factorial of " + num + " = " + factorial);
+    }
+    public static long multiplyNumbers(int num)
+    {
+        if (num >= 1)
+            return num * multiplyNumbers(num - 1);
+        else
+            return 1;
+    }
+}
