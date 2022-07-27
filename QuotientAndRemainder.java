c7a937bfb0c1596f510b50f55925757022dda97b
vaishu009 <109944617+vaishu009@users.noreply.github.com>
2022-07-27 11:52:41 +0530

    Program to Find Quotient and Remainder

---
+++ QuotientAndRemainder.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kanev
+ */
+public class QuotientAndRemainder {
+    public static void main(String[] args) {
+		float dividend=30,divisor=5,quotient,remainder;
+		
+		quotient=dividend/divisor;
+		remainder=dividend%divisor;
+		
+		System.out.println("Quotient = "+quotient);
+		System.out.println("Remainder = "+remainder);
+		
+		
+	}
+}
