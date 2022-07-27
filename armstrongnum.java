a4e6de4c0a8a6f378641ad570579372763ea2846
vaishu009 <109944617+vaishu009@users.noreply.github.com>
2022-07-27 11:57:09 +0530

    Armstrong Number Between Two Intervals

---
+++ armstrongnum.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaprograms;
+
+/**
+ *
+ * @author kanev
+ */
+public class armstrongnum {
+    public static void main(String[] args) {
+int low = 999, high = 99999;
+for(int number = low + 1; number < high; ++number) {
+if (checkArmstrong(number))
+System.out.print(number + " ");
+}
+}
+public static boolean checkArmstrong(int num) {
+int digits = 0;
+int result = 0;
+int originalNumber = num;
+// number of digits calculation
+while (originalNumber != 0) {
+originalNumber /= 10;
+++digits;
+}
+originalNumber = num;
+// result contains sum of nth power of its digits
+while (originalNumber != 0) {
+int remainder = originalNumber % 10;
+result += Math.pow(remainder, digits);
+originalNumber /= 10;
+}
+if (result == num)
+return true;
+return false;
+}
+}
