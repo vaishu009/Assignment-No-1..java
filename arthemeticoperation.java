65a9454efc062dabef1bb20bda7e4512a0d5121b
vaishu009 <109944617+vaishu009@users.noreply.github.com>
2022-07-27 11:58:06 +0530

    Program to Add, subtract, multiply and divide

---
+++ arthemeticoperation.java
@@ -0,0 +1,20 @@
+/*
+Program to Add, subtract, multiply and divide Two Numbers
+ */
+public class arthemeticoperation {
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        int c;
+        
+        c=a+b;
+        System.out.println("addtion : "+c);
+        c=a-b;
+        System.out.println("Substraction : "+c);
+        c=a/b;
+        System.out.println("Division : "+c);
+        c=a*b;
+        System.out.println("Multiplication : "+c);
+    }
+}
