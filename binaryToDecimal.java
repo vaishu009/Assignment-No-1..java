97e5fb7c79f47b27358af239a75dfd329621d9c5
vaishu009 <109944617+vaishu009@users.noreply.github.com>
2022-07-27 11:59:01 +0530

    convert Binary Number to Decimal and vice-versa

---
+++ binaryToDecimal.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaprograms;
+
+/**
+ *
+ * @author kanev
+ */
+public class binaryToDecimal {
+     public static void main(String[] args) {
+
+    // binary number
+    long num = 110110111;
+
+    // call method by passing the binary number
+    int decimal = convertBinaryToDecimal(num);
+
+    System.out.println("Binary to Decimal");
+    System.out.println(num + " = " + decimal);
+  }
+
+  public static int convertBinaryToDecimal(long num) {
+    int decimalNumber = 0, i = 0;
+    long remainder;
+    
+    while (num != 0) {
+      remainder = num % 10;
+      num /= 10;
+      decimalNumber += remainder * Math.pow(2, i);
+      ++i;
+    }
+    
+    return decimalNumber;
+  }
+}
