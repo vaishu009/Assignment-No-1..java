c621feac32092724f217eafc0a87769c331afa82
vaishu009 <109944617+vaishu009@users.noreply.github.com>
2022-07-27 11:24:01 +0530

    Java Program to Find ASCII Value of a Character

---
+++ ASCIValues.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kanev
+ */
+public class ASCIValues {
+    public static void main(String[] args) {
+
+        char ch = 'a';
+        int ascii = ch;
+        // You can also cast char to int
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+}
