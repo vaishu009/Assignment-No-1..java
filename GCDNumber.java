d3ab5e1c684302ad576fbcda04093014c02e4c5b
vaishu009 <109944617+vaishu009@users.noreply.github.com>
2022-07-27 11:31:39 +0530

    Java Program to Find GCD

---
+++ GCDNumber.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kanev
+ */
+public class GCDNumber {
+    public static void main(String[] args)   
+{  
+int n1=50, n2=60;  
+while(n1!=n2)   
+{  
+if(n1>n2)  
+n1=n1-n2;  
+else  
+n2=n2-n1;  
+}  
+System.out.printf("GCD of n1 and n2 is: " +n2);  
+}  
+}
