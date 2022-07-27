b995c416f5396cef64b82dbe53d21cd13f6d3176
vaishu009 <109944617+vaishu009@users.noreply.github.com>
2022-07-27 11:30:07 +0530

    Display Factors of a Number

---
+++ FactorOfNum.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaprograms;
+
+/**
+ *
+ * @author kanev
+ */
+public class FactorOfNum {
+     public static void main(String[] args) {
+
+    // positive number
+    int number = 60;
+
+    System.out.print("Factors of " + number + " are: ");
+
+    // loop runs from 1 to 60
+    for (int i = 1; i <= number; ++i) {
+
+      // if number is divided by i
+      // i is the factor
+      if (number % i == 0) {
+        System.out.print(i + " ");
+      }
+    }
+  }
+}
