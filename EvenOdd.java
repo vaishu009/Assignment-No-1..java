b4674bdf36209dd1a7b08c3ff2da58ffdae02c3d
vaishu009 <109944617+vaishu009@users.noreply.github.com>
2022-07-27 11:29:29 +0530

    Check Whether Number is Even or Odd

---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+/*
+Program to Check Whether Number is Even or Odd
+ */
+public class EvenOdd {
+    public static void main(String args[])
+    {
+        int num=11;
+        
+        if(num%2==0)
+        {
+            System.out.println("Number is even");
+        }
+        else
+        {
+            System.out.println("Number is Odd");
+        }
+    }
+}
