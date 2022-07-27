6fa3d2f4b51b6fb0e1c3dd6d23f8369955ccb998
vaishu009 <109944617+vaishu009@users.noreply.github.com>
2022-07-27 11:26:51 +0530

    Find Size of int, float, double and char

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+/*
+Program to Find Size of int, float, double and char in Your System
+ */
+public class DataTypes {
+     public static void main (String[] args)
+ {
+  System.out.println("Size of int: " + (Integer.SIZE/8) + " bytes.");
+  System.out.println("Size of long: " + (Long.SIZE/8) + " bytes.");
+  System.out.println("Size of char: " + (Character.SIZE/8) + " bytes.");
+  System.out.println("Size of float: " + (Float.SIZE/8) + " bytes.");
+  System.out.println("Size of double: " + (Double.SIZE/8) + " bytes.");
+  }
+}
