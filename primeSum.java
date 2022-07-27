bb848b09e308983212e5f1a18c2f3d7d421280ad
vaishu009 <109944617+vaishu009@users.noreply.github.com>
2022-07-27 12:04:40 +0530

    Number can be Express as Sum of Two Prime Numbers

---
+++ primeSum.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaprograms;
+
+/**
+ *
+ * @author kanev
+ */
+public class primeSum {
+
+  public static void main(String[] args) {
+    int number = 34;
+    boolean flag = false;
+    for (int i = 2; i <= number / 2; ++i) {
+
+      // condition for i to be a prime number
+      if (checkPrime(i)) {
+
+        // condition for n-i to be a prime number
+        if (checkPrime(number - i)) {
+
+          // n = primeNumber1 + primeNumber2
+          System.out.printf("%d = %d + %d\n", number, i, number - i);
+          flag = true;
+        }
+
+      }
+    }
+
+    if (!flag)
+      System.out.println(number + " cannot be expressed as the sum of two prime numbers.");
+  }
+
+  // Function to check prime number
+  static boolean checkPrime(int num) {
+    boolean isPrime = true;
+
+    for (int i = 2; i <= num / 2; ++i) {
+      if (num % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+
+    return isPrime;
+  }
+}
+
