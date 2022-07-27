bc8834184a7670f998ea85afc404ed1fe0638e11
vaishu009 <109944617+vaishu009@users.noreply.github.com>
2022-07-27 11:52:03 +0530

    Find All Roots of a Quadratic Equation

---
+++ QuadraticEquation.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kanev
+ */
+public class QuadraticEquation {
+    public static void main(String[] args) {
+
+    // value a, b, and c
+    double a = 2.3, b = 4, c = 5.6;
+    double root1, root2;
+
+    // calculate the determinant (b2 - 4ac)
+    double determinant = b * b - 4 * a * c;
+
+    // check if determinant is greater than 0
+    if (determinant > 0) {
+
+      // two real and distinct roots
+      root1 = (-b + Math.sqrt(determinant)) / (2 * a);
+      root2 = (-b - Math.sqrt(determinant)) / (2 * a);
+
+      System.out.format("root1 = %.2f and root2 = %.2f", root1, root2);
+    }
+
+    // check if determinant is equal to 0
+    else if (determinant == 0) {
+
+      // two real and equal roots
+      // determinant is equal to 0
+      // so -b + 0 == -b
+      root1 = root2 = -b / (2 * a);
+      System.out.format("root1 = root2 = %.2f;", root1);
+    }
+
+    // if determinant is less than zero
+    else {
+
+      // roots are complex number and distinct
+      double real = -b / (2 * a);
+      double imaginary = Math.sqrt(-determinant) / (2 * a);
+      System.out.format("root1 = %.2f+%.2fi", real, imaginary);
+      System.out.format("\nroot2 = %.2f-%.2fi", real, imaginary);
+    }
+  }
+}
